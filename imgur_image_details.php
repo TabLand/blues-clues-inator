9f64cff6f5f8dc4f6d90737e99b28482c75f56dd
Ijtaba Hussain <ijtabahussain@live.com>
2015-01-24 19:48:05 -0500

    Libify functions

--- imgur_image_details.php
+++ imgur_image_details.php
@@ -1,5 +1,6 @@
 <?php
     include "imgur.php";
+    include "imgur_lib.php";
     error_reporting(E_ALL);
 
     $imgur_link = $_GET["imgur"];
@@ -9,29 +10,4 @@
 
     header("application/json");
     echo $json;
-
-    function get_imgur_id($link){
-        $bits      = explode("/",$link);
-        $imgur_id  = array_pop( $bits );
-        preg_match("/[A-Za-z0-9]+/",$imgur_id,$matches);
-
-        $clean_id = $matches[0];
-        return $clean_id;
-    }
-
-    function get_image_details($img_id){
-        $api_url = "https://api.imgur.com/3/image/$img_id";
-        
-        $handle = curl_init($api_url);
-
-        $auth_header = "Authorization: Client-ID " . CLIENT_ID;
-
-        curl_setopt($handle, CURLOPT_HTTPHEADER, array( $auth_header ) );
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-
-        $json = curl_exec($handle);
-
-        curl_close($handle);
-        return $json;
-    }
 ?>
---
+++ imgur_lib.php
@@ -0,0 +1,26 @@
+<?php
+    function get_imgur_id($link){
+        $bits      = explode("/",$link);
+        $imgur_id  = array_pop( $bits );
+        preg_match("/[A-Za-z0-9]+/",$imgur_id,$matches);
+
+        $clean_id = $matches[0];
+        return $clean_id;
+    }
+
+    function get_image_details($img_id){
+        $api_url = "https://api.imgur.com/3/image/$img_id";
+
+        $handle = curl_init($api_url);
+
+        $auth_header = "Authorization: Client-ID " . CLIENT_ID;
+
+        curl_setopt($handle, CURLOPT_HTTPHEADER, array( $auth_header ) );
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+        $json = curl_exec($handle);
+
+        curl_close($handle);
+        return $json;
+    }
+?>
