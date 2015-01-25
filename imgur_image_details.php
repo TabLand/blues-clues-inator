e829abe820c4458006707dbaea70178370cd04e3
Ijtaba Hussain <tab@ijtaba.me.uk>
2015-01-24 19:33:48 -0500

    Improved id grabbing function

--- imgur_image_details.php
+++ imgur_image_details.php
@@ -11,12 +11,12 @@
     echo $json;
 
     function get_imgur_id($link){
-        $pattern  = "/https?:\/\/[www]?imgur\.com\/gallery\//";
-        $imgur_id = preg_replace($pattern,"",$link);
-        preg_match("/[A-Za-z]+/",$imgur_id,$matches);
+        $bits      = explode("/",$link);
+        $imgur_id  = array_pop( $bits );
+        preg_match("/[A-Za-z0-9]+/",$imgur_id,$matches);
 
         $clean_id = $matches[0];
-        return $matches[0];
+        return $clean_id;
     }
 
     function get_image_details($img_id){
